6561a86f8a7ed94a771bc0a38fdd1e4179b1d5e1
yuta-hi <hiasa.yuta.ht7@is.naist.jp>
2020-03-30 21:48:20 +0900

    update updaters

--- pytorch_bcnn/updaters/cgan/__init__.py
+++ pytorch_bcnn/updaters/cgan/__init__.py
@@ -2,9 +2,9 @@ from __future__ import absolute_import
 
 from abc import ABCMeta, abstractmethod
 
-from chainer.training import StandardUpdater
-from chainer import reporter
-from chainer.dataset import convert
+from pytorch_trainer.training import StandardUpdater
+from pytorch_trainer import reporter
+from pytorch_trainer.dataset import convert
 
 
 def _update(optimizer, in_arrays, loss_func):
@@ -25,48 +25,42 @@ class CGANUpdater(StandardUpdater, metaclass=ABCMeta):
         optimizer (dict): Optimizers to update parameters. It should be a dictionary
             that has `gen` and `dis` keys. Note that `gen` and `dis` means the generator
             and discniminator, respectively.
+        model (dict): Generative and discriminative models. It should be a dictionary
+            that has `gen` and `dis` keys. Note that `gen` and `dis` means the generator
+            and discniminator, respectively.
         alpha (float): Loss scaling factor for balancing the conditional loss.
         converter (optional): Converter function to build input arrays. Defaults to `convert.concat_examples`.
         device (int, optional): Device to which the training data is sent. Negative value
             indicates the host memory (CPU). Defaults to None.
         loss_func: Conditional loss function. `lossfun` attribute of the optimizer's target link for
             the generator is used by default. Defaults to None.
-        auto_new_epoch (bool, optional): If ``True``,
-            :meth:`~chainer.Optimizer.new_epoch` of optimizers is
-            automatically called when the ``is_new_epoch`` attribute of the
-            main iterator is ``True``. Defaults to True.
     """
 
 
 
-    def __init__(self, iterator, optimizer, alpha,
+    def __init__(self, iterator, optimizer, model, alpha,
                     converter=convert.concat_examples,
-                    device=None, loss_func=None, auto_new_epoch=True):
+                    device=None, loss_func=None):
 
         assert isinstance(optimizer, dict)
 
-        loss_scale = None
-
         super(CGANUpdater, self).__init__(
-            iterator, optimizer, converter,
-            device, loss_func, loss_scale, auto_new_epoch)
+            iterator, optimizer, model, converter,
+            device, loss_func)
 
         self.alpha = alpha
 
     @property
     def discriminator(self):
-        optimizer = self._optimizers['dis']
-        return optimizer.target
+        return self._models['dis']
 
     @property
     def generator(self):
-        optimizer = self._optimizers['gen']
-        return optimizer.target
+        return self._models['gen']
 
     def conditional_lossfun(self, y_fake, y_true):
 
-        optimizer = self._optimizers['gen']
-        model = optimizer.target
+        model = self.generator
 
         if hasattr(model, 'lossfun'):
             lossfun = model.lossfun
--- pytorch_bcnn/updaters/cgan/dcgan.py
+++ pytorch_bcnn/updaters/cgan/dcgan.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import
 
-import chainer.functions as F
-from chainer import reporter
-from chainer.dataset import convert
+import numpy
+import torch
+import torch.nn.functional as F
+from pytorch_trainer import reporter
+from pytorch_trainer.dataset import convert
 
 from . import CGANUpdater
 from ._replay_buffer import ReplayBuffer
@@ -15,6 +17,9 @@ class DCGANUpdater(CGANUpdater):
         optimizer (dict): Optimizers to update parameters. It should be a dictionary
             that has `gen` and `dis` keys. Note that `gen` and `dis` means the generator
             and discniminator, respectively.
+        model (dict): Generative and discriminative models. It should be a dictionary
+            that has `gen` and `dis` keys. Note that `gen` and `dis` means the generator
+            and discniminator, respectively.
         alpha (float): Loss scaling factor for balancing the conditional loss.
         buffer_size (int, optional): Size of buffer, which handles the experience replay. Defaults to 0.
         converter (optional): Converter function to build input arrays. Defaults to `convert.concat_examples`.
@@ -22,35 +27,31 @@ class DCGANUpdater(CGANUpdater):
             indicates the host memory (CPU). Defaults to None.
         loss_func: Conditional loss function. `lossfun` attribute of the optimizer's target link for
             the generator is used by default. Defaults to None.
-        auto_new_epoch (bool, optional): If ``True``,
-            :meth:`~chainer.Optimizer.new_epoch` of optimizers is
-            automatically called when the ``is_new_epoch`` attribute of the
-            main iterator is ``True``. Defaults to True.
 
     See also:
         https://arxiv.org/pdf/1511.06434.pdf
     """
-    def __init__(self, iterator, optimizer, alpha, buffer_size=0,
+    def __init__(self, iterator, optimizer, model, alpha, buffer_size=0,
                     converter=convert.concat_examples,
-                    device=None, loss_func=None, auto_new_epoch=True):
+                    device=None, loss_func=None):
 
         super(DCGANUpdater, self).__init__(
-            iterator, optimizer, alpha, converter,
-            device, loss_func, auto_new_epoch)
+            iterator, optimizer, model, alpha, converter,
+            device, loss_func)
 
         self._buffer = ReplayBuffer(buffer_size)
         self._buffer_size = buffer_size
 
     def discriminative_lossfun(self, p_real, p_fake):
-        size = p_real.size // p_real.shape[1]
-        loss = (F.sum(F.softplus(-p_real)) / size \
-                + F.sum(F.softplus(p_fake)) / size) * 0.5 # NOTE: equivalent to binary cross entropy
+        size = p_real.numel() / p_real.shape[1]
+        loss = (torch.sum(F.softplus(-p_real)) / size \
+                + torch.sum(F.softplus(p_fake)) / size) * 0.5 # NOTE: equivalent to binary cross entropy
         reporter.report({'loss_dis': loss})
         return loss
 
     def generative_lossfun(self, p_fake):
-        size = p_fake.size // p_fake.shape[1]
-        loss = F.sum(F.softplus(-p_fake)) / size
+        size = p_fake.numel() / p_fake.shape[1]
+        loss = torch.sum(F.softplus(-p_fake)) / size
         reporter.report({'loss_gen': loss})
         return loss
 
@@ -63,37 +64,40 @@ class DCGANUpdater(CGANUpdater):
         opt_dis = self._optimizers['dis']
         opt_gen = self._optimizers['gen']
 
+        for model in self._models.values():
+            model.train()
+
         x_real, y_real = in_arrays
 
         # generative
+        self.discriminator.requires_grad_(False)
+
         y_fake = self.generator(x_real)
-        xy_fake = F.concat((x_real, y_fake))
+        xy_fake = torch.cat((x_real, y_fake), dim=1)
         p_fake = self.discriminator(xy_fake)
 
         loss_gen = self.generative_lossfun(p_fake) \
                     + self.alpha * self.conditional_lossfun(y_fake, y_real)
 
-        self.generator.cleargrads()
+        opt_gen.zero_grad()
         loss_gen.backward()
-        opt_gen.update()
+        opt_gen.step()
 
         # discriminative
         # NOTE: deallocate intermediate variable nodes related to the generator
-        #       with `array` method instead of `unchain_backward`
-        y_fake_old = self._buffer(y_fake.array)
+        #       with `detach` method
+        self.discriminator.requires_grad_(True)
 
-        xy_fake = F.concat((x_real, y_fake_old))
+        y_fake_old = self._buffer(y_fake.detach())
+
+        xy_fake = torch.cat((x_real, y_fake_old), dim=1)
         p_fake = self.discriminator(xy_fake)
 
-        xy_real = F.concat((x_real, y_real))
+        xy_real = torch.cat((x_real, y_real), dim=1)
         p_real = self.discriminator(xy_real)
 
         loss_dis = self.discriminative_lossfun(p_real, p_fake)
 
-        self.discriminator.cleargrads()
+        opt_dis.zero_grad()
         loss_dis.backward()
-        opt_dis.update()
-
-        if self.auto_new_epoch and iterator.is_new_epoch:
-            opt_gen.new_epoch(auto=True)
-            opt_dis.new_epoch(auto=True)
+        opt_dis.step()
